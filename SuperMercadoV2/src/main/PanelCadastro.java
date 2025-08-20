e32c547bb938118017a3b4c0cabf3b080103932d
Vitor Hugo Prange <vitor.p2007@aluno.ifsc.edu.br>
2025-08-20 08:39:43 -0300

    Nova versão com codigo feito em sala com o professor

---
+++ SuperMercadoV2/src/main/PanelCadastro.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class PanelCadastro extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelCadastro() {
+
+	}
+
+}
